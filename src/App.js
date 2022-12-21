264af18cad3064f28b4acdae55b2e0f0b3057d08
German <gtumorin@gmail.com>
2022-12-21 11:56:11 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { useState } from "react";
 import { Table } from "react-bootstrap";
 
 const App = ({ list }) => {
+  const keys = Object.keys(list[0]);
   const [isSortIncrease, setSortDirect] = useState(true);
-  const [keys, setKeys] = useState(Object.keys(list[0]));
   const [tableRows, setTableRows] = useState(list);
 
-  const clickHandle = (e) => {
+  const clickHandle = (index) => {
+    const [...newTableRows] = tableRows;
+    const key = keys[index];
+    isSortIncrease
+      ? newTableRows.sort((user1, user2) => (user1[key] > user2[key] ? 1 : -1))
+      : newTableRows.sort((user1, user2) => (user1[key] < user2[key] ? 1 : -1));
+    setTableRows(newTableRows);
     const newSortDirection = !isSortIncrease;
     setSortDirect(newSortDirection);
   };
@@ -20,7 +26,7 @@ const App = ({ list }) => {
       <thead>
         <tr>
           {keys.map((key, index) => (
-            <th onClick={clickHandle} key={index} id={index}>
+            <th onClick={() => clickHandle(index)} key={index} id={index}>
               {key}
             </th>
           ))}
