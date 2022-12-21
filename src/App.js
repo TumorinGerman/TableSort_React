24877d7758b597babbffa08828c256f6c6d562b4
German <gtumorin@gmail.com>
2022-12-21 10:11:38 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,28 +2,32 @@ import React, { useState } from "react";
 import { Table } from "react-bootstrap";
 
 const App = ({ list }) => {
+  const [isSortIncrease, setSortDirect] = useState(true);
+  const [keys, setKeys] = useState(Object.keys(list[0]));
+  const [tableRows, setTableRows] = useState(list);
+
   const clickHandle = (e) => {
-    console.log(e.target);
+    const newSortDirection = !isSortIncrease;
+    setSortDirect(newSortDirection);
   };
 
-  const [sortDirect, setSortDirect] = useState(null);
   if (list.length === 0) {
     return null;
   }
-  const keys = Object.keys(list[0]);
+
   return (
     <Table striped bordered hover variant="dark">
       <thead>
         <tr>
           {keys.map((key, index) => (
-            <th onClick={clickHandle} key={index}>
+            <th onClick={clickHandle} key={index} id={index}>
               {key}
             </th>
           ))}
         </tr>
       </thead>
       <tbody>
-        {list.map((row, index) => (
+        {tableRows.map((row, index) => (
           <tr key={index}>
             {keys.map((key, index) => (
               <td key={index}>{row[key]}</td>
